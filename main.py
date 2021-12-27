5f4b0929a74ba5aadfc5fd6a9d3196548886c90b
Jorge <54640013+jorgeemejia@users.noreply.github.com>
2021-12-27 15:00:41 -0800

    Update main.py

--- main.py
+++ main.py
@@ -22,19 +22,4 @@ async def on_message(message):
     await message.channel.send("Hello")
 
 
-#MEMBER_JOIN :
-
-#The first step of creating the 'member_join' functionality is figuring out a way to see when someone joins the discord server.
-#So that's what we'll be figuring out first.
-
-#TO DO:
-# Looking at the structure of the client events above, create another client event that is called when a member joins the discord server
-# HINT: ctrl+f "on_member_join" in the discord.py API Reference https://discordpy.readthedocs.io/en/stable/api.html
-
-# Make it so whenever someone joins the discord server, a statement is outputted in the terminal.
-# (You can simulate someone joining your server by inviting another bot to your server.
-# There are tons of sites with tons of bots, just choose one and have them join)
-# Here is a link to a site that let's you invite bots to your server: https://top.gg/  
-
-
 client.run(os.environ.get('TOKEN'))
