f8b4ae71b0b7576114ea1ef855d71d21a342535b
nicholascaro <70615231+nicholascaro@users.noreply.github.com>
2022-01-04 15:08:02 -0800

    Successfully tested async def on_member_join

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 intents = discord.Intents.default()
@@ -8,18 +9,17 @@ intents.members = True
 client = discord.Client(intents=intents)
 
 
-
-@client.event  #creates a client event
-async def on_ready():  #event is called using the on_ready function
-  """Outputs a statement to the terminal that the bot has logged on successfully"""
-  print('logged in as {0.user}'.format(client)) #outputs a statement to the terminal
+@client.event  # creates a client event
+async def on_ready():  # event is called using the on_ready function
+    """Outputs a statement to the terminal that the bot has logged on successfully"""
+    print('logged in as {0.user}'.format(client))  # outputs a statement to the terminal
 
 
 @client.event
 async def on_message(message):
-  """Whenever a user says 'Hi', the bot will respond with 'Hello'"""
-  if message.content == "Hi":
-    await message.channel.send("Hello")
+    """Whenever a user says 'Hi', the bot will respond with 'Hello'"""
+    if message.content == "Hi":
+        await message.channel.send("Hello")
 
 
 client.run(os.environ.get('TOKEN'))
