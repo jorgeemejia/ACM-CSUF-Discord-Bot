bbe08ee625a3837f0225779c905b59a6ffb8a124
jorgeemejia <jorgeemejia@csu.fullerton.edu>
2022-01-03 19:49:23 -0800

    Added a To Do: section and reverted the token's variable name

--- events/On_Member_Join.py
+++ events/On_Member_Join.py
@@ -25,5 +25,22 @@ async def on_message(message):
 async def on_member_join(member):
   print("A new member has joined!!")
 
+# Now that we have a basic understanding of client events, let's go a little further.
+# As of now, when a member joins, a message is outputted into the terminal. Our goal now,
+# is to assign a member to a role once they join. We want to assign the new member to a role where they
+# have very few permissions. 
 
-client.run(os.environ.get('Himani'))
+# TO DO:
+# - Create a role in your discord server
+# - Alter the body of your 'on_member_join' client event to meet our goal
+
+# Hints/Help:
+# - If you aren't familiar with creating roles on Discord -> https://support.discord.com/hc/en-us/articles/206029707-Setting-Up-Permissions-FAQ
+# - CTRL+F "discord.utils.get" on the Discord.py API Reference, you'll use this function to store your desired role into a variable
+# - CTRL+F "add_roles" on the Discord.py API Reference, you'll use this function to add a member to your desired role
+# - "discord.utils.get" might seem tricky to understand at first but there are a few examples of how to use it in the 
+# documentation
+
+#Good Luck :)
+
+client.run(os.environ.get('TOKEN'))
