cc40f94e893d029d830e2f39ad5b298fe62bb382
AaronLieb <aarnliebrmn@gmail.com>
2022-01-06 18:59:19 -0800

    Handle cases where the guild is not int he env file

--- cogs/schedules.py
+++ cogs/schedules.py
@@ -3,11 +3,19 @@ from discord.ext.commands import Bot, Cog
 from discord_slash import cog_ext, SlashContext
 from os import environ
 
+
+def getGuilds():
+    guild = environ.get('GUILD')
+    if guild is not None:
+        return [int(guild)]
+    else:
+        return []
+
 class Schedules(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    @cog_ext.cog_slash(name="test", guild_ids=[int(environ.get('GUILD'))])
+    @cog_ext.cog_slash(name="test", guild_ids=getGuilds())
     async def _test(self, ctx: SlashContext):
         embed = Embed(title="Embed Test")
         await ctx.send(embed=embed)
