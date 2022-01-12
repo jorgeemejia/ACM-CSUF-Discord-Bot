0b0415d9fbe2b500cda3854a6c692d4fd742568e
AaronLieb <aarnliebrmn@gmail.com>
2022-01-11 20:35:50 -0800

    comments for schedule commands

--- cogs/schedules.py
+++ cogs/schedules.py
@@ -11,18 +11,6 @@ class Schedules(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    def fetchGoogleFormResults():
-        # Fetch the google form results every 15s
-        # process the new results, and pass them to the confirmUser function 
-        pass
-
-    def confirmUser(user, classes):
-        # Find the user by discord tag
-        # DM them the classes, check that this is the correct user, and the correct classes
-        # Display the message using discord's embedded messages
-        # Have a user click a button to confirm everything is correct
-        pass
-
 
     @cog_ext.cog_slash(name="test", guild_ids=guild_ids)
     async def _test(self, ctx: SlashContext):
@@ -31,6 +19,32 @@ class Schedules(Cog):
     @cog_ext.cog_slash(name="test2", description="This command has a description", guild_ids=guild_ids)
     async def _test2(self, ctx: SlashContext):
         await ctx.send(content="test2")
+
+    # for aaron to do:
+        # code for subcommands
+        # code for json example
+        # embed help
+
+    # two json files, classes.json, students.json
+
+    # /schedule 
+        # open the students.json file
+        # return the students classes
+        # display button asking to share to the channel
+
+    # /schedule add
+        # open both json files
+        # validate the input
+        # add the class to the person
+        # add the person to the class
+        # give them the role/permissions
+        
+    # /schedule remove
+        # open both json files
+        # check if the student is in that class
+        # remove the class from the student
+        # remove the student from the class
+        # update their roles/permissions
     
     """
     Create your own command that allows a user to run /schedule, and it will return a link to the google form ("https://docs.google.com/forms/d/1sz-YZ3QkpTkVMFYlX8Mt0k1jhJrSY1BPzXMMUVdCEXs/edit")
