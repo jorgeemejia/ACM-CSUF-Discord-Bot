ac4e52625f963d1b3a8deb091ae4f7dfbd7afb65
AaronLieb <aarnliebrmn@gmail.com>
2022-01-11 17:44:34 -0800

    Fixed cog slash commands

--- cogs/schedules.py
+++ cogs/schedules.py
@@ -2,6 +2,7 @@ from discord import Embed
 from discord.ext.commands import Bot, Cog
 from discord_slash import cog_ext, SlashContext
 from os import environ
+guild_ids = [914688600828960788]
 
 def getGuilds():
     guildList = environ.get('GUILD')
@@ -15,10 +16,13 @@ class Schedules(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    @cog_ext.cog_slash(name="test", guild_ids=getGuilds())
+    @cog_ext.cog_slash(name="test", guild_ids=guild_ids)
     async def _test(self, ctx: SlashContext):
-        embed = Embed(title="Embed Test")
-        await ctx.send(content="", embed=embed)
+        await ctx.send(content="test")
+
+    @cog_ext.cog_slash(name="test2", description="This command has a description", guild_ids=guild_ids)
+    async def _test2(self, ctx: SlashContext):
+        await ctx.send(content="test2")
 
 def setup(bot: Bot):
     bot.add_cog(Schedules(bot))
