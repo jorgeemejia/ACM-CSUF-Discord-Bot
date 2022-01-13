1c603107413954f3d46bc353200165b0f8a6df3b
janiljohn <56321852+janiljohn@users.noreply.github.com>
2022-01-12 20:28:13 -0800

    Added instructions

--- cogs/schedules.py
+++ cogs/schedules.py
@@ -41,7 +41,35 @@ class Schedules(Cog):
     Create your own command that allows a user to run /schedule, and it will return a link to the google form ("https://docs.google.com/forms/d/1sz-YZ3QkpTkVMFYlX8Mt0k1jhJrSY1BPzXMMUVdCEXs/edit")
 
     Create a description for the command as well.
+
     """
+
+    # for aaron to do:
+        # code for subcommands
+        # code for json example
+        # embed help
+
+    # two json files, classes.json, students.json
+
+    # /schedule 
+        # open the students.json file
+        # return the students classes
+        # display button asking to share to the channel
+
+    # /schedule add
+        # open both json files
+        # validate the input
+        # add the class to the person
+        # add the person to the class
+        # give them the role/permissions
+
+    # /schedule remove
+        # open both json files
+        # check if the student is in that class
+        # remove the class from the student
+        # remove the student from the class
+        # update their roles/permissions
+
 
 def setup(bot: Bot):
     bot.add_cog(Schedules(bot))
