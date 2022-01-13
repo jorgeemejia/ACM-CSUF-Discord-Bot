f1747dd161d20d2d6d9aa4da55bbf7a99da0bbe2
janiljohn <56321852+janiljohn@users.noreply.github.com>
2022-01-13 00:14:52 -0800

    Added functionality for /schedule view

--- cogs/schedules.py
+++ cogs/schedules.py
@@ -4,6 +4,7 @@ from discord_slash import cog_ext, SlashContext
 from os import path
 import json
 from helpers import getGuilds
+import discord
 
 guild_ids = getGuilds()
 
@@ -41,7 +42,16 @@ class Schedules(Cog):
 
     @cog_ext.cog_subcommand(base="schedule", name="view", description="View your schedule", guild_ids=guild_ids)
     async def _schedule_view(self, ctx: SlashContext):
-        await ctx.send(content="schedule")
+        embed=discord.Embed(title="Hello " + ctx.author.display_name + "!", 
+    description="Here are your classes:", 
+    color=0xFF5733)
+        embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
+        classes = students[ctx.author.display_name]
+        for class_name in classes:
+            idx = class_name.index("-")
+            class_session = class_name[idx+1:]
+            embed.add_field(name=class_name[:idx], value="Session: " + class_session, inline=False)
+        await ctx.send(embed=embed)
 
     @cog_ext.cog_subcommand(base="schedule", name="add", description="Add a class to your schedule", guild_ids=guild_ids)
     async def _schedule_add(self, ctx: SlashContext, text: str):
